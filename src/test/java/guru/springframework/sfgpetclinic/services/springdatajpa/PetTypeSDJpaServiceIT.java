24cc599e3c7966beaab6be09c782a6a6d8db5fb5
Illya Sokolov <illya.sokolov82@gmail.com>
2021-05-12 15:49:54 +0300

    adding extension example

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -1,11 +1,14 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import junitextensions.TimingExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /* IT - integration test. See future tasks */
+@ExtendWith(TimingExtension.class)
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
